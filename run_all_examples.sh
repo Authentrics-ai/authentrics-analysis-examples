d262ca26b395e7ca23419bf3468d4842d6361b44
Michael Jacobs <michael.jacobs@authentrics.ai>
2026-03-18 17:08:35 +0000

    enhance: update run_all_examples.sh to prompt for login and improve project directory handling

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-for set in torch hf onnx; do
-    for f in examples/$set/*.py; do
-        script_name="${f##*/}"
-        if [ "$script_name" == "__init__.py" ]; then
+echo "Make sure to login before running the examples"
+echo "If you are not logged in cancel this and run: authrx login"
+read -p "Press Enter to continue or Ctrl+C to cancel"
+
+for set in torch onnx hf; do
+    cd examples/$set
+
+    for f in *.py; do
+        echo "Running $f"
+        python $f
+    
+        project_dir=$(ls -d my_*project | head -n 1)
+        if [ -z "$project_dir" ]; then
             continue
         fi
-        echo "Running $script_name"
-        python $f
+    
+        authrx project delete $project_dir
+        rm -rf $project_dir
     done
+    
+    cd ../..
 done
